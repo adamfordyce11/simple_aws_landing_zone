d015b85bce1ff88cb08dc767a34c9224f5221c68
Adam Fordyce <adamfordyce@hotmail.com>
2022-11-18 13:23:50 +0000

    Added common variables.tf

---
+++ common/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "AWS region"
+}
+variable "stateBucketName" {
+  description = "S3 Bucket Name"
+}
+variable "stateBucketKey" {
+  description = "S3 Bucket Key"
+}
+variable "stateBucketLockTable" {
+  description = "S3 Bucket Lock Table"
+}
