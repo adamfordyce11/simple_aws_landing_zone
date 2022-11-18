ed0229598c5bd82b914d81cdef3961ba5f617b9c
Adam Fordyce <adamfordyce@hotmail.com>
2022-11-18 13:23:27 +0000

    Added common data.tf

---
+++ common/data.tf
@@ -0,0 +1,10 @@
+
+data "terraform_remote_state" "state" {
+  backend = "s3"
+  config {
+    bucket     = var.stateBucketName
+    lock_table = var.stateBucketLockTable
+    region     = var.stateBucketRegion
+    key        = var.stateBucketKey
+  }
+}
