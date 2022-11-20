b0d7d7d4494b2a3d8cfa243ecbe1d22a65893431
Adam Fordyce <adamfordyce@hotmail.com>
2022-11-20 00:47:36 +0000

    [Issue #1]: Updated common tf files

--- common/terraform.tf
+++ common/terraform.tf
@@ -1,9 +1,12 @@
 terraform {
-    backend "s3" {}
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = ">= 3.20.0"
-        }
+  required_version = ">= 0.12"
+  
+  backend "s3" { }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.20.0"
     }
+  }
 }
--- common/variables.tf
+++ common/variables.tf
@@ -1,12 +1,8 @@
+variable account {
+  description = "Platform Account"
+}
+
 variable "region" {
   description = "AWS region"
-}
-variable "stateBucketName" {
-  description = "S3 Bucket Name"
-}
-variable "stateBucketKey" {
-  description = "S3 Bucket Key"
-}
-variable "stateBucketLockTable" {
-  description = "S3 Bucket Lock Table"
+  default     = "eu-west-1"
 }
